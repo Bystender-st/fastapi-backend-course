b1ce67c55ef2e67d78ad6fafc1f6193befc939cd
St Win <stalsghost@gmail.com>
2025-08-01 18:50:25 +1000

    Improve main.py: add API for task managment
    A simple API for managing a list of tasks has been created. Data is stored in RAM as a Python dictionary.
    
    Functions added:
    - get_tasks: returns a list of all tasks
    - create_task: creates a new task
    - update_task: updates task information
    - delete_task: deletes a task
    
    Each task has parameters: id, name, and status.

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,19 +1,35 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
+dict_of_tasks = {}
+tasks_id_counter = 0
+
 @app.get("/tasks")
 def get_tasks():
-    pass
+    return dict_of_tasks
 
 @app.post("/tasks")
-def create_task(task):
-    pass
+def create_task(task: str, status: str = "--New task--"):
+    global tasks_id_counter
+    if task not in {t["task"] for t in dict_of_tasks.values()}:
+        dict_of_tasks[tasks_id_counter] = {"task" : task, "status" : status}
+        tasks_id_counter += 1
+        return f"Created id[{tasks_id_counter - 1}]: task: {task}, status: {status}"
 
 @app.put("/tasks/{task_id}")
-def update_task(task_id: int):
-    pass
+def update_task(task_id: int, task: str, status: str = "--Updated task--"):
+    if task_id in dict_of_tasks:
+        dict_of_tasks[task_id] = {"task" : task, "status" : status}
+        return f"{dict_of_tasks[task_id]} updated!"
+    else:
+        return f"Error, task with id[{task_id}] did not found!"
 
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
-    pass
+    if task_id in dict_of_tasks:
+        dict_of_tasks.pop(task_id)
+        return f"Task with id[{task_id}] deleted!"
+    else:
+        return f"Error, task with id[{task_id}] did not found!"
